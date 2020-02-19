96c2c1bc6dc3ab5c04479efa316b546a8601afe4
Your Name <you@example.com>
2020-02-19 18:04:20 +0100

    first commt

---
+++ src/main/java/com/example/serveurproduitstock/ServeurproduitstockApplication.java
@@ -0,0 +1,13 @@
+package com.example.serveurproduitstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurproduitstockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeurproduitstockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serveurproduitstock/ServeurproduitstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serveurproduitstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurproduitstockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
